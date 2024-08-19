f5dc2e07aee8d33e27428ca79a72bf38e8a635f0
manases24 <hugo.orlando2050@gmail.com>
2024-08-19 10:23:39 -0300

    multiple inputs added

--- 03-advanced-projects/src/App.tsx
+++ 03-advanced-projects/src/App.tsx
@@ -1,11 +1,12 @@
 // import { UserChallenge } from "./tutorial/06-forms/02-user-challenge";
+// import { MultipleInputs } from "./tutorial/06-forms/03-multiple-inputs";
 
-import { MultipleInputs } from "./tutorial/06-forms/03-multiple-inputs";
+import { OtherInputs } from "./tutorial/06-forms/04-other-inputs";
 
 export function App() {
   return (
     <div className="container">
-      <MultipleInputs/>
+      <OtherInputs/>
     </div>
   );
 }
--- 03-advanced-projects/src/tutorial/06-forms/04-other-inputs.tsx
+++ 03-advanced-projects/src/tutorial/06-forms/04-other-inputs.tsx
@@ -1,23 +1,59 @@
-import { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 
 const frameworks = ["react", "angular", "vue", "svelte"];
 
 export const OtherInputs = () => {
+  const [shipping, setShipping] = useState<boolean>(false);
+  const [framework, setFramework] = useState<string>("react");
+
+  const handleShipping = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked);
+  };
+
+  const handleFramework = (e: ChangeEvent<HTMLSelectElement>) => {
+    setFramework(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Aquí puedes manejar el envío del formulario
+    console.log({ shipping, framework });
+  };
+
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Other Inputs</h4>
-        {/* name */}
         <div className="form-row" style={{ textAlign: "left" }}>
+          <input
+            type="checkbox"
+            checked={shipping}
+            id="shipping"
+            name="shipping"
+            onChange={handleShipping}
+          />
           <label htmlFor="shipping"> Free Shipping </label>
         </div>
         <div className="form-row" style={{ textAlign: "left" }}>
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((fw) => (
+              <option key={fw} value={fw}>
+                {fw}
+              </option>
+            ))}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
-          submit
+          Submit
         </button>
       </form>
     </div>
