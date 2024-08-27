6af1766ba56fd0f271b1a9550f81f645396263c8
manases24 <hugo.orlando2050@gmail.com>
2024-08-27 14:08:56 -0300

    context api with dark mode example and styles css added

--- 03-advanced-projects/src/App.tsx
+++ 03-advanced-projects/src/App.tsx
@@ -2,12 +2,19 @@
 // import { MultipleInputs } from "./tutorial/06-forms/03-multiple-inputs";
 // import { OtherInputs } from "./tutorial/06-forms/04-other-inputs";
 // import { UncontrolledInputs } from "./tutorial/06-forms/05-form-data";
-import { UserContainer } from "./tutorial/09-context-api/prop-drilling/UserContainer";
+
+import Theme from "./tutorial/09-context-api/context/dark-mode/ThemeProvider";
+import { Navbar } from "./tutorial/09-context-api/context/Navbar";
+
+
+// import { UserContainer } from "./tutorial/09-context-api/context/UserContainer";
+
 
 export function App() {
   return (
     <div className="container">
-      <UserContainer user={user} logout={logout}/>
+     {/* <Navbar/> */}
+     <Theme/>
     </div>
   );
 }
