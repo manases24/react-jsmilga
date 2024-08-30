8caffedc280181f398ddd29f56336192714a0987
manases24 <hugo.orlando2050@gmail.com>
2024-08-30 16:25:59 -0300

    menu added

---
+++ 04-fundamental-projects/05-menu/src/App.tsx
@@ -0,0 +1,13 @@
+import { Title } from "./Title"
+
+function App() {
+  return (
+    <main>
+      <section className="menu">
+        <Title title="our menu"/>
+      </section>
+    </main>
+  )
+}
+
+export default App
