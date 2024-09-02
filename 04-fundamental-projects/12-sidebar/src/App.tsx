8aaf1354a00565c1b2d636d4cab72bc0f5a08433
manases24 <hugo.orlando2050@gmail.com>
2024-09-02 14:07:06 -0300

    sidebar added

--- 04-fundamental-projects/12-sidebar/src/App.tsx
+++ 04-fundamental-projects/12-sidebar/src/App.tsx
@@ -1,14 +1,15 @@
+import { AppProvider } from "./context";
 import Home from "./Home";
 import { Modal } from "./Modal";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <Home />
       <Modal />
       <Sidebar />
-    </>
+    </AppProvider>
   );
 }
 
--- 04-fundamental-projects/12-sidebar/src/main.tsx
+++ 04-fundamental-projects/12-sidebar/src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { AppProvider } from "./context.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <AppProvider>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </AppProvider>
-);
+  </StrictMode>,
+)
