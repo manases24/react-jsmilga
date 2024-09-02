73edf5fbe0d2a3070f43ed22dc7454a48535351d
manases24 <hugo.orlando2050@gmail.com>
2024-09-02 17:34:20 -0300

    react query added

--- 06-react-query/src/SingleItem.tsx
+++
@@ -1,5 +0,0 @@
-export const SingleItem = () => {
-  return (
-    <div>SingleItem</div>
-  )
-}
--- 06-react-query/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const customFetch = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
-});
