61b5fc736e3804223c47a0a779de3dde6807eef5
manases24 <hugo.orlando2050@gmail.com>
2024-09-02 13:42:03 -0300

    api added

---
+++ 04-fundamental-projects/12-sidebar/src/api/types.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export type LinkType = {
+  id: number;
+  url: string;
+  text: string;
+  icon: ReactNode;
+};
+
+export type SocialType = {
+  id: number;
+  url: string;
+  icon: ReactNode;
+};
