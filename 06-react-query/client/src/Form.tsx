69a2a66958199b7211e618c45d2bc34f285aebdd
manases24 <hugo.orlando2050@gmail.com>
2024-09-04 13:17:43 -0300

    react query added

--- 06-react-query/client/src/Form.tsx
+++ 06-react-query/client/src/Form.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
-import { useCreateTask } from './services/hooks';
+import { useState } from "react";
+import { useCreateTask } from "./services/hooks";
 
 export const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+  const [newItemName, setNewItemName] = useState("");
   const { createTask, isLoading } = useCreateTask();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    createTask({ taskTitle: newItemName }, {
-      onSuccess: () => {
-        setNewItemName('');
-      },
-    });
+    createTask(
+      { taskTitle: newItemName },
+      {
+        onSuccess: () => {
+          setNewItemName("");
+        },
+      }
+    );
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h4>Task Builder</h4>
-      <div className='form-control'>
+      <h4>task bud</h4>
+      <div className="form-control">
         <input
-          type='text'
-          className='form-input'
+          type="text"
+          className="form-input"
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn' disabled={isLoading}>
+        <button type="submit" className="btn" disabled={isLoading}>
           Add Task
         </button>
       </div>
