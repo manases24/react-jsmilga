e2780749c4d80826033b9faa91f5429f2ca15707
manases24 <hugo.orlando2050@gmail.com>
2024-09-07 15:02:16 -0300

    mixmaster

--- 09-mixmaster/src/pages/Error.tsx
+++ 09-mixmaster/src/pages/Error.tsx
@@ -1,5 +1,27 @@
+import { Link, useRouteError } from "react-router-dom";
+import { Wrapper } from "../assets/wrappers/ErrorPage";
+import img from "../assets/not-found.svg";
+
 export const Error = () => {
+  const error: any = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find page you are looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>Error</div>
-  )
-}
+    <Wrapper>
+      <div>
+        <h3>something went wrong </h3>
+      </div>
+    </Wrapper>
+  );
+};
