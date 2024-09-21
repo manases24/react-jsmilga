a05ef1fbd5b579db31151fc4a6820dac2f68b011
manases24 <hugo.orlando2050@gmail.com>
2024-09-21 14:12:49 -0300

    cocktail search ap

--- 09-mixmaster/src/components/SearchForm.tsx
+++ 09-mixmaster/src/components/SearchForm.tsx
@@ -1,5 +1,22 @@
-export const SearchForm = ({searchTerm}:any) => {
+import { Form, useNavigation } from "react-router-dom";
+import { Wrapper } from "../assets/wrappers/SearchForm";
+
+export const SearchForm = ({ searchTerm }: any) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <div>SearchForm</div>
-  )
-}
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue={searchTerm}
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "searching..." : "search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
+};
--- 09-mixmaster/src/pages/Cocktail.tsx
+++ 09-mixmaster/src/pages/Cocktail.tsx
@@ -1,28 +1,15 @@
+import axios from "axios";
 import { useLoaderData, Navigate, useNavigate } from "react-router-dom";
 import { Wrapper } from "../assets/wrappers/CocktailPage";
 import { QueryClient, useQuery } from "@tanstack/react-query";
 import { Drink, LoaderData } from "../api/types";
-import axios from "axios";
 
-// const singleCocktailQuery = (id: string) => {
-//   return {
-//     queryKey: ["cocktail", id],
-//     queryFn: async () => {
-//       const { drinks } = await axiosSingleCocktail.get<CocktailDBResponse>(
-//         `${id}`
-//       );
-//       console.log(drinks);
-//       return drinks;
-//     },
-//   };
-// };
 const singleCocktailUrl =
-  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
-
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const singleCocktailQuery = (id: string) => {
   return {
-    queryKey: ['cocktail', id],
+    queryKey: ["cocktail", id],
     queryFn: async () => {
       const { data } = await axios.get(`${singleCocktailUrl}${id}`);
       return data;
@@ -44,10 +31,22 @@ export const Cocktail = () => {
 
   if (!id) return <Navigate to="/" />;
 
-  const { data } = useQuery(singleCocktailQuery(id));
+  const { data, isLoading, isError } = useQuery(singleCocktailQuery(id));
 
-  if (!data || data.length === 0) return <Navigate to="/" />;
-  const singleDrink = data[0];
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading data...</p>;
+  }
+
+  // Check if 'data.drinks' exists and is not empty
+  if (!data || !data.drinks || data.drinks.length === 0) {
+    return <Navigate to="/" />;
+  }
+
+  const singleDrink = data.drinks[0];
 
   const {
     strDrink: name,
@@ -56,8 +55,6 @@ export const Cocktail = () => {
     strCategory: category,
     strGlass: glass,
     strInstructions: instructions,
-    strTags: tags,
-    strVideo: video,
   } = singleDrink;
 
   const validIngredients = Object.keys(singleDrink)
@@ -79,40 +76,26 @@ export const Cocktail = () => {
         <img src={image} alt={name} className="img" />
         <div className="drink-info">
           <p>
-            <span className="drink-data">name :</span>
-            {name}
+            <span className="drink-data">name :</span> {name}
           </p>
           <p>
-            <span className="drink-data">category :</span>
-            {category}
+            <span className="drink-data">category :</span> {category}
           </p>
           <p>
-            <span className="drink-data">info :</span>
-            {info}
+            <span className="drink-data">info :</span> {info}
           </p>
           <p>
-            <span className="drink-data">glass :</span>
-            {glass}
+            <span className="drink-data">glass :</span> {glass}
           </p>
-          <p>
-            <span className="drink-data">Tags:</span> {tags}
-          </p>
-          {video && (
-            <p>
-              <span className="drink-data">Video:</span>
-              <a href={video}>Watch here</a>
-            </p>
-          )}
+
           <p>
             <span className="drink-data">ingredients :</span>
-            {validIngredients.map((item, index) => {
-              return (
-                <span className="ing" key={item}>
-                  {item}
-                  {index < validIngredients.length - 1 ? "," : ""}
-                </span>
-              );
-            })}
+            {validIngredients.map((item, index) => (
+              <span className="ing" key={item}>
+                {item}
+                {index < validIngredients.length - 1 ? "," : ""}
+              </span>
+            ))}
           </p>
           <p>
             <span className="drink-data">instructions :</span>
