bbd8023c6d50530210f505fcf883279703b49796
manases24 <hugo.orlando2050@gmail.com>
2024-09-23 14:46:09 -0300

    pages are added

---
+++ 12-comfy-store/src/pages/index.ts
@@ -0,0 +1,11 @@
+export { About } from "./About";
+export { Cart } from "./Cart";
+export { Checkout } from "./Checkout";
+export { Error } from "./Error";
+export { HomeLayout } from "./HomeLayout";
+export { Landing } from "./Landing";
+export { Login } from "./Login";
+export { Orders } from "./Orders";
+export { Products } from "./Products";
+export { Register } from "./Register";
+export { SingleProduct } from "./SingleProduct";
