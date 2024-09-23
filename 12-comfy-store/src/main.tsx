4f36e23dac7a5b9c4a362e04a059fd2e74eb38df
manases24 <hugo.orlando2050@gmail.com>
2024-09-23 15:30:09 -0300

    redux toolkit are added

--- 12-comfy-store/src/main.tsx
+++ 12-comfy-store/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import { store } from './redux/store.ts'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    <ToastContainer position='top-center' />
+  </Provider>
 )
